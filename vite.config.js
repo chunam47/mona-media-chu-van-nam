8f24622b55a91889d2bb247bd25e24214276851f
Chu Văn Nam <chunam47@gmail.com>
2022-04-18 20:29:42 +0700

    config

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/mona-media-chu-van-nam",
+  base: "/mona-media-chu-van-nam/",
   plugins: [react()],
 });
